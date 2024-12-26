cf30b188cb5d74b9c6c4b1ebe8b39c15c79dd8a5
Abhivanya <your_email@example.com>
2024-12-26 12:35:51 +0530

    statring news app suing nextjs

---
+++ pages/aboutus/index.js
@@ -0,0 +1,5 @@
+const AboutusPage = () => {
+  return <div>index</div>;
+};
+
+export default AboutusPage;
---
+++ pages/news/index.js
@@ -0,0 +1,5 @@
+const Newspage = () => {
+  return <div>News page</div>;
+};
+
+export default Newspage;
